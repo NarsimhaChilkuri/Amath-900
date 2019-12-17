1f100f8854e28e9f105a800009d717c41ca609d7
Narsimha Reddy Chilkuri <44869051+NarsimhaChilkuri@users.noreply.github.com>
2019-12-17 12:02:33 -0500

    Update visual_noise.py

--- visual_noise.py
+++ visual_noise.py
@@ -16,17 +16,17 @@ logging.console.setLevel(logging.DEBUG)
 
 win = visual.Window([224, 224], allowGUI=False, color=(-1, -1, -1))
 
-noiseTexture = numpy.random.rand(40, 40) * 2.0 - 1
+noiseTexture = numpy.random.rand(16, 16) * 2.0 - 1
 
 patch = visual.GratingStim(win, tex=noiseTexture,
-    size=(64, 64), units='pix', mask="circle",
+    size=(32, 32), units='pix', mask="circle",
     interpolate=True, autoLog=False, blendmode="avg", ori=0, pos=(-10, 15))
     
 #patch = visual.NoiseStim(win, mask='circle', units='pix', pos=(0.0, 0.0), size=(128, 128), noiseType="binary", noiseElementSize=32, interpolate=True, blendmode='avg')
 
 while not event.getKeys():
     # increment by (1, 0.5) pixels per frame:
-    patch.phase += (-1 / 16.0, 0)
+    patch.phase += (-1 / 300.0, 0)
     patch.pos += (0, -1)
     patch.draw()
     win.flip()
